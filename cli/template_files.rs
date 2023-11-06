6375f771a70f6ac153e7e1cb377da3667f788413
Max Thomson <git@mnt.dev>
2023-11-05 19:58:12 -0800

    [Cli] Ignore GH workflow files

--- cli/template_files.rs
+++ cli/template_files.rs
@@ -5,6 +5,15 @@ use tera::{Context, Tera};
 
 use std::fs;
 
+fn should_ignore_file(file: &str) -> bool {
+    // Ignore GH workflow files
+    if file.ends_with(".yml") && file.contains(".github/workflows/") {
+        return true;
+    }
+
+    return false;
+}
+
 pub fn templates_files<T: RustEmbed, S: Serialize>(project_name: String, context: &S) {
     for file in T::iter() {
         let filename = file.as_ref().to_string();
@@ -14,17 +23,21 @@ pub fn templates_files<T: RustEmbed, S: Serialize>(project_name: String, context
         let file = T::get(&filename).unwrap().data;
         let filecontents = std::str::from_utf8(file.as_ref()).unwrap();
 
-        let rendered_contents = Tera::one_off(
-            filecontents,
-            &Context::from_serialize(context).expect("Cannot serialize struct"),
-            true,
-        )
-        .unwrap();
-        if rendered_contents.trim().is_empty() {
-            continue;
-        }
-
         fs::create_dir_all(Path::new(&filepath).parent().unwrap()).ok();
-        fs::write(filepath, rendered_contents).expect("Couldn't write file");
+
+        if should_ignore_file(&filepath) {
+            fs::write(filepath, filecontents).expect("Couldn't write file");
+        } else {
+            let rendered_contents = Tera::one_off(
+                filecontents,
+                &Context::from_serialize(context).expect("Cannot serialize struct"),
+                true,
+            )
+            .unwrap();
+            if rendered_contents.trim().is_empty() {
+                continue;
+            }
+            fs::write(filepath, rendered_contents).expect("Couldn't write file");
+        }
     }
 }
--- cli/utils.rs
+++ cli/utils.rs
@@ -8,14 +8,14 @@ where
     <T as FromStr>::Err: std::fmt::Debug,
 {
     // Choose template
-    let template_selections: Vec<String> = T::iter().map(|x| return x.to_string()).collect();
+    let enums: Vec<String> = T::iter().map(|x| return x.to_string()).collect();
 
-    let template = Select::with_theme(&ColorfulTheme::default())
+    let choosen_enum: usize = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("Template?")
         .default(0)
-        .items(&template_selections[..])
+        .items(&enums[..])
         .interact()
         .unwrap();
 
-    return T::from_str(&template_selections[template]).unwrap();
+    return T::from_str(&enums[choosen_enum]).unwrap();
 }
